bfc864940292750b93e4a5b3a9f581c99af83408
dongyeopchae <fghvbn456123@naver.com>
2025-10-13 05:05:39 +0900

    1-1 게임 종료 구현
     게임 종료와 Restart 버튼 구현

---
+++ Assets/Script/goal.cs
@@ -0,0 +1,33 @@
+using Unity.Cinemachine;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goal : MonoBehaviour
+{
+    public GameObject gameover;
+    public GameObject restart;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+}
+
+// Update is called once per frame
+void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+            Time.timeScale = 0;
+            gameover.SetActive(true);
+            restart.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Stage1-1");
+    }
+}
